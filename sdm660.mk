0e3635740cb24c851ca813c16c0ff2a8e2e9b65e
Sujin Panicker <spanic@codeaurora.org>
2021-06-27 19:32:53 +0600

    sdm660-common: Enable soundtrigger hidl hal 2.3 - soundtrigger hidl hal 2.3 for SP with kernel version 4.19.
    Signed-off-by: OdSazib <odsazib@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -99,7 +99,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.3-impl \
     audio.a2dp.default \
     audio.primary.sdm660 \
     audio.r_submix.default \
