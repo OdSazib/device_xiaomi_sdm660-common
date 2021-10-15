aafabf4a7b7e80e02fe2afead6b9d5930fcb9ff7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-15 13:50:54 +0800

    sdm660-common: Uprev bluetooth audio HIDL impl to 2.1 * We get free LC3 support, isn't it poggers?
    Change-Id: I0f1713ee542102374183ef8088bcad62d3859c2f
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: OdSazib <odsazib@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -126,7 +126,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.bluetooth@1.1.vendor \
     bt_stack.conf \
     liba2dpoffload \
