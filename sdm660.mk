2339a5465ff5230c55a17ab17275b4443f909ec9
Edwin Wong <edwinwong@google.com>
2021-10-15 11:42:56 +0800

    sdm660-common: Update clearkey plugin to drm HAL v1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I406b7d3c2921d4698535421894c6eb3785eb1c0d
    [clarencelol: Uprev manifest and also vendor interfaces]
    
    Change-Id: I4c219e27e5cd92aab66c8abc082785cc29afc6bc

--- sdm660.mk
+++ sdm660.mk
@@ -187,8 +187,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3-service.clearkey \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
