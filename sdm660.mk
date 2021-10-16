c21b27b489697ae344b2050f489ed23d9c66793b
Marc Bourgoin <themard85@gmail.com>
2021-10-17 00:21:31 +0800

    sdm660-common: Drop QCOM WFD - Keep vendor/lib64/libmmosal.so for vendor/lib64/libqcc_file_agent.so
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: OdSazib <odsazib@gmail.com>
    Change-Id: Iea4fc5e1c089eecab407fadb9911900f9fd07e9a

--- sdm660.mk
+++ sdm660.mk
@@ -593,12 +593,3 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(COMMON_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
-
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libaacwrapper \
-    libnl
-
-# WiFi Display JAR
-# PRODUCT_BOOT_JARS += \
-#    WfdCommon
