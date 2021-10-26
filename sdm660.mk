e52ce56826fea9aee4ae3aa30862ab11686de5b4
orges <me@orgesified.ml>
2021-10-26 21:25:34 +0800

    sdm660-common: Add display config file
     * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back
     * DisplayDeviceConfig: requesting nits when no mapping exists.
    
    Signed-off-by: orges <me@orgesified.ml>
    Change-Id: I1716319bb17b9236d07d44a2a7fa3c7640c54ddf
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: OdSazib <odsazib@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -205,6 +205,10 @@ PRODUCT_PACKAGES += \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor
 
+# Display Device Config
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/displayconfig/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
 # Device Parts
 PRODUCT_PACKAGES += \
     DeviceDoze \
