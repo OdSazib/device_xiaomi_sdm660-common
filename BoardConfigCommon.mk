788c578f46633203a3d3f212bebe6be086337cad
OdSazib <odsazib@gmail.com>
2021-11-13 19:48:10 +0600

    sdm660-common: Nuke neverallows flag from sepolicy - Fixed all neverallows after merging LA.UM.10.2.1.r1-02700-sdm660.0 to legacy-um
    Signed-off-by: OdSazib <odsazib@gmail.com>
    Change-Id: Ib028a0fb3066616609865c128266b634db43b0ac

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -220,7 +220,6 @@ include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
-SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 PRODUCT_VENDOR_MOVE_ENABLED := true
