4f322c5f16c01c1e604cd8cc035e6dd4bc8b0e3b
OdSazib <odsazib@gmail.com>
2022-03-11 17:36:01 +0600

    sdm660-common: sepolicy: Update to new qcom-sepolicy
    Changes:
    - Label idle_state nodes
    - Removed many nodes from device sepolicy as caf labeled them at LA.UM.10.2.1.r1-03400-sdm660.0
    - Removed devicesettings redundants and duplicate hwservice
    - Use SYSTEM_EXT_{PRIVATE/PUBLIC}_SEPOLICY_DIRS instead of BOARD_PLAT_{PRIVATE/PUBLIC}_SEPOLICY_DIRS
    
    Change-Id: I094bf639fd2f1f4625b19378c03dc31fed150aa6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -202,8 +202,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 
 # SurfaceFlinger
 TARGET_USE_AOSP_SURFACEFLINGER := true
